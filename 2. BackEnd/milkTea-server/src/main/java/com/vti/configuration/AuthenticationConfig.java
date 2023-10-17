e8aea6b2d6437de453d009047456eaa72140e51f
hyelinh <leehyelinh.1989@gmail.com>
2023-10-18 00:43:10 +0900

    config model mapper

--- 2. BackEnd/milkTea-server/src/main/java/com/vti/configuration/AuthenticationConfig.java
+++ 2. BackEnd/milkTea-server/src/main/java/com/vti/configuration/AuthenticationConfig.java
@@ -1,5 +1,7 @@
 package com.vti.configuration;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -8,8 +10,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class AuthenticationConfig {
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    public ModelMapper modelMapper() {
+        // Tạo object và cấu hình
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.STRICT);
+        return modelMapper;
     }
 
 }
